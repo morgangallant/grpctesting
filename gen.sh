a7c04532956942347b36a66b7d21b5e9bf5967e4
Morgan Gallant <morgan@morgangallant.com>
2021-12-22 15:19:02 -0500

    fix gen
    Signed-off-by: Morgan Gallant <morgan@morgangallant.com>

--- gen.sh
+++ gen.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 rm -f pb/*.pb.go
-go install google.golang.org/protobuf/cmd/protoc-gen-go
-go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+go install \
+    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway \
+    github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-openapiv2 \
+    google.golang.org/protobuf/cmd/protoc-gen-go \
+    google.golang.org/grpc/cmd/protoc-gen-go-grpc
 for proto in pb/azad/*.proto; do
 	protoc -I pb/ $proto --go_out=pb/ --go-grpc_out=pb/ --grpc-gateway_out=logtostderr=true:pb/
 done
